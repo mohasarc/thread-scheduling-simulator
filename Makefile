54b1b5b04c5d6f87c7765432a0c48fb6859bf9ab
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-03-26 17:06:32 +0300

    got the usleep to work

--- Makefile
+++ Makefile
@@ -1,14 +1,5 @@
-all: schedule # producer consumer
-
-# producer: ./src/producer.c
-# 	gcc -Wall -g -o producer ./src/producer.c
-
-# consumer: ./src/consumer.c
-# 	gcc -Wall -g -o consumer ./src/consumer.c
-
 schedule: ./src/schedule.c
-	gcc -Wall -g -o schedule ./src/schedule.c ./src/run_queue/pcb.c ./src/run_queue/linked_list.c -lm
+	gcc -Wall -g -o schedule ./src/schedule.c ./src/run_queue/pcb.c ./src/run_queue/linked_list.c -lm -pthread 
 
 clean:
 	rm -fr schedule 
-	#producer consumer *dSYM
--- src/schedule.c
+++ src/schedule.c
@@ -3,9 +3,10 @@
 #include <math.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 // #include <sys/types.h>
 // #include <sys/wait.h>
-// #include <sys/time.h>
+#include <sys/time.h>
 #include <time.h>
 // #include <fcntl.h> 
 #include "./run_queue/linked_list.h"
@@ -15,6 +16,25 @@
 #define FILE_EXTENTION ".txt"
 #define READ_FROM_FILE_OPT "-f"
 
+// GLOBAL VARIABLES
+int N;      // The number of W threads.
+int Bcount; // The number of bursts that each W thread will generate.
+int minB;   // Minimum random value. (if less regenerate)
+int avgB;   // The parameter of the exponentially distributed random 
+            // value of the burst time.
+int minA;   // Minimum random value. (if less regenerate)
+int avgA;   // The parameter of the exponentially distributed random 
+            // value of the interarrival time (sleep time of W threads).
+char* ALG;  // The schedulingalgorithm to use: “FCFS”,  “SJF”, “PRIO”, “VRUNTIME”.
+
+struct LL_NODE* RQ_HEAD = NULL; // The ready queue head
+struct LL_NODE* RQ_TAIL = NULL; // The ready queue tail
+
+
+/**
+ * Generates an exponantially distributed random number
+ * @param mean
+ * */
 double getExpRandomNum(int mean ){
     double rn;
     double lambda = 1 / (double)mean;
@@ -26,18 +46,47 @@ double getExpRandomNum(int mean ){
     return log((1-rn))/(-lambda);
 }
 
+/**
+ * The job a W thread does
+ * @param t_index The index of the thread
+ * */
+void *doWJob(void* t_index){
+    int interarrival_time;
+    int b_length;
+    struct timeval wall_clock_time;
+    long wall_clock_time_usec;
+    srand ( time(NULL) );
+    
+    for (int i = 1; i <= Bcount; i++)
+    {
+        // Generate random numbers
+        interarrival_time = getExpRandomNum(avgA);
+        b_length = getExpRandomNum(avgB);
+
+        // Sleep
+        usleep(interarrival_time*1000);
+
+        // Schedule self
+        gettimeofday(&wall_clock_time, NULL);
+        wall_clock_time_usec = (wall_clock_time.tv_sec * 1000) + wall_clock_time.tv_usec/1000;
+        struct PCB_DATA *pcb_data = create_pcb_data(*((int *) t_index), i, b_length, wall_clock_time_usec);
+        LL_shift(pcb_data, &RQ_HEAD, &RQ_TAIL);
+    }
+
+    return NULL;
+}
+
+/**
+ * The job an S thread does
+ * @param param 
+ * */
+void *doSJob(void* param){
+    return NULL;
+}
+
 int main(int argc, char *argv[])
-{  
-    // LOCAL VARIABLES
-    int N;      // The number of W threads.
-    int Bcount; // The number of bursts that each W thread will generate.
-    int minB;   // Minimum random value. (if less regenerate)
-    int avgB;   // The parameter of the exponentially distributed random 
-                // value of the burst time.
-    int minA;   // Minimum random value. (if less regenerate)
-    int avgA;   // The parameter of the exponentially distributed random 
-                // value of the interarrival time (sleep time of W threads).
-    char* ALG;  // The schedulingalgorithm to use: “FCFS”,  “SJF”, “PRIO”, “VRUNTIME”.
+{
+    // LOCAL VARIABLES  
     int readFromFile = FALSE;
     char* fileName;
 
@@ -68,55 +117,19 @@ int main(int argc, char *argv[])
         // Read data from file
     }
 
-    printf("Reading from file: %d, fileName: %s \n", readFromFile, fileName);
-    printf("Reading params: N %d, Bcount %d, minB %d, avgB %d, minA %d, avgA %d, ALG %s \n", N, Bcount, minB, avgB, minA, avgA, ALG);
+    // int result = usleep(1000003);
+    // int result = nanosleep((struct timespec[]){{0, 500000000L}},NULL);
+    // printf("usleep returned: %d\n",result);
 
-    srand ( time(NULL) );
-    printf("exp rand num %f \n", getExpRandomNum(30));
+    // Create the threads
+    pthread_t tid;
+    pthread_attr_t attr;
 
+    int *arg = (int *)malloc(sizeof(int));
+    *arg = 5;
+    pthread_attr_init(&attr);
+    pthread_create(&tid, &attr, doWJob, arg);
+    pthread_join(tid, NULL);
 
-    // Pointers to the linked list head/ tail
-    struct LL_NODE* LINKED_LIST_HEAD = NULL;
-    struct LL_NODE* LINKED_LIST_TAIL = NULL;
-
-    // 
-    // LL_push(4, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_push(3, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_push(42, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_push(8, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    struct PCB_DATA *removedValue = NULL;
-
-    struct PCB_DATA * data1 = create_pcb_data(1, 3, 5, 21);
-    struct PCB_DATA * data2 = create_pcb_data(4, 1, 6, 12);    
-    struct PCB_DATA * data3 = create_pcb_data(5, 2, 5, 13);
-    struct PCB_DATA * data4 = create_pcb_data(2, 1, 5, 23);
-    
-    LL_shift(data1, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    LL_shift(data2, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    LL_shift(data3, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    LL_shift(data4, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-
-    LL_unshift(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_pop(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_pop(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_pop(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-
-    // LL_unshift(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_pop(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_unshift(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_unshift(&removedValue, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-  
-  
-    // LL_remove(LINKED_LIST_HEAD, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-    // LL_remove(LINKED_LIST_TAIL, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-
-    // LL_remove_by_value(555, &LINKED_LIST_HEAD, &LINKED_LIST_TAIL);
-
-    LL_print(LINKED_LIST_HEAD, LINKED_LIST_TAIL);
-    printf("removed value: %d\n", removedValue->b_index);
-
-    destroy_pcb(data1);
-    destroy_pcb(data2);
-    destroy_pcb(data3);
-    destroy_pcb(data4);
+    LL_print(RQ_HEAD, RQ_TAIL);
 }
